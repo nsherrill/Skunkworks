0a1afddf95f63c93611fafda472cefd038f7748c
Norm Sherrill <nsherrill@nebraskaglobal.com>
2018-07-25 23:16:05 -0500

    initial push

---
+++ RFKofLancaster/Dashboard/Controllers/VolunteerController.cs
@@ -0,0 +1,31 @@
+﻿using Dashboard.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Dashboard.Controllers
+{
+    public class VolunteerController : Controller
+    {
+        // GET: Volunteer
+        public ActionResult Index()
+        {
+            List<VolunteerModel> result = new List<VolunteerModel>();
+            result.Add(new VolunteerModel()
+            {
+                VolunteerId = 1,
+                Name = "Nick",
+                NickName = "Nick"
+            });
+            result.Add(new VolunteerModel()
+            {
+                VolunteerId = 2,
+                Name = "Kevin",
+                NickName = "Kevin"
+            });
+            return View(result.ToArray());
+        }
+    }
+}
---
+++ RFKofLancaster/Dashboard/Models/VolunteerModels.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Dashboard.Models
+{
+    [Table("Volunteers")]
+    public class VolunteerModel
+    {
+        [Required]
+        [Key]
+        [Display(Name = "Id")]
+        public int VolunteerId { get; set; }
+
+        [Required]
+        [Display(Name = "Name")]
+        public string Name { get; set; }
+
+        [Required]
+        [Display(Name = "NickName")]
+        public string NickName { get; set; }
+    }
+    public class CounselorModel : VolunteerModel
+    {
+    }
+}
